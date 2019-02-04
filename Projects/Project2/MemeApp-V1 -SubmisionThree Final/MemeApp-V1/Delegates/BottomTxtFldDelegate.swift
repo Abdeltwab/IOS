75a0c388b706e9431660d6f397f489d45a9e1254
Abdeltwab Elhussin <Abdeltwab>
2019-02-04 03:59:48 +0200

    Submision three

---
+++ Projects/Project2/MemeApp-V1 -SubmisionThree Final/MemeApp-V1/Delegates/BottomTxtFldDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  BottomTxtFldDelegate.swift
+//  MemeApp-V1
+//
+//  Created by Abdeltwab Elhussin on 2/2/19.
+//  Copyright © 2019 Abdeltwab Elhussin. All rights reserved.
+//
+
+import UIKit
+
+class BottomTxtFldDelegate: NSObject , UITextFieldDelegate{
+
+    // LifeCycle
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return true
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
+}
